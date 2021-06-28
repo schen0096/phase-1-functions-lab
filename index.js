3acc4e98f977e8b94e310e81f6824c327c8a3e47
steven chen <schen0096@gmail.com>
2021-06-28 17:15:17 -0400

    commit tree 0cf00437f44492d098e9ca3a39ddd0cbba0c8ae9

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const hq = 42
+const ft = 264
+
+function distanceFromHqInBlocks(num){
+    return Math.abs(num - hq)
+}
+
+function distanceFromHqInFeet(num){
+    return (Math.abs(num - hq ) * ft) 
+}
+
+function distanceTravelledInFeet(num1, num2){
+    return (Math.abs(num1 - num2) * ft)
+}
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) < 400){
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000){
+        return ((Math.abs(start - destination) * ft) - 400) * 0.02
+    }  
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500){
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+
+}
